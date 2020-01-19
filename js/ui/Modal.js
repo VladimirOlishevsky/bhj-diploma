f6431cdbbb4d163c91e902fea1a950c02a8a4c60
VladimirOlishevsky <vova.olishevskij@inbox.ru>
2020-01-19 18:39:04 +0300

    add 1

--- js/ui/Modal.js
+++ js/ui/Modal.js
@@ -5,50 +5,63 @@
  * закрытие имеющихся окон
  * */
 class Modal {
-  /**
-   * Устанавливает текущий элемент в свойство element
-   * Регистрирует обработчики событий с помощью
-   * AccountsWidget.registerEvents()
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * */
-  constructor( element ) {
+    /**
+     * Устанавливает текущий элемент в свойство element
+     * Регистрирует обработчики событий с помощью
+     * AccountsWidget.registerEvents()
+     * Если переданный элемент не существует,
+     * необходимо выкинуть ошибку.
+     * */
+    constructor(element) {
+        if (element === '') {
+            return Error
+        };
+        this.element = element;
+        this.registerEvents()
+    }
 
-  }
+    /**
+     * При нажатии на элемент с data-dismiss="modal"
+     * должен закрыть текущее окно
+     * (с помощью метода Modal.onClose)
+     * */
+    registerEvents() {
 
-  /**
-   * При нажатии на элемент с data-dismiss="modal"
-   * должен закрыть текущее окно
-   * (с помощью метода Modal.onClose)
-   * */
-  registerEvents() {
+        let add = function() {
+            this.element.onClose()
+        }
+        for (let i = 0; i < this.element.querySelectorAll('[data-dismiss]').length; i++) {
+            this.element.querySelectorAll('[data-dismiss]')[i].addEventListener('click', add)
+        }
+    }
 
-  }
-
-  /**
-   * Срабатывает после нажатия на элементы, закрывающие окно.
-   * Закрывает текущее окно (Modal.close())
-   * */
-  onClose( e ) {
-
-  }
-  /**
-   * Удаляет обработчики событий
-   * */
-  unregisterEvents() {
-
-  }
-  /**
-   * Открывает окно: устанавливает CSS-свойство display
-   * со значением «block»
-   * */
-  open() {
-
-  }
-  /**
-   * Закрывает окно: удаляет CSS-свойство display
-   * */
-  close(){
-
-  }
+    /**
+     * Срабатывает после нажатия на элементы, закрывающие окно.
+     * Закрывает текущее окно (Modal.close())
+     * */
+    onClose(e) {
+            this.element.close()
+            e.preventDefault()
+        }
+        /**
+         * Удаляет обработчики событий
+         * */
+    unregisterEvents() {
+            for (let i = 0; i < this.element.querySelectorAll('[data-dismiss]').length; i++) {
+                this.querySelectorAll('[data-dismiss]')[i].removeEventListener('click', add)
+            }
+        }
+        /**
+         * Открывает окно: устанавливает CSS-свойство display
+         * со значением «block»
+         * */
+    open() {
+            this.element.display = 'block';
+        }
+        /**
+         * Закрывает окно: удаляет CSS-свойство display
+         * */
+    close() {
+        this.element.display = 'none';
+    }
 }
--- js/ui/forms/AsyncForm.js
+++ js/ui/forms/AsyncForm.js
@@ -6,44 +6,60 @@
  * для последующей обработки
  * */
 class AsyncForm {
-  /**
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * Сохраняет переданный элемент и регистрирует события
-   * через registerEvents()
-   * */
-  constructor( element ) {
+    /**
+     * Если переданный элемент не существует,
+     * необходимо выкинуть ошибку.
+     * Сохраняет переданный элемент и регистрирует события
+     * через registerEvents()
+     * */
+    constructor(element) {
+        if (element === '') {
+            return Error
+        };
+        this.element = element;
+        this.registerEvents()
+    }
 
-  }
+    /**
+     * Необходимо запретить отправку формы. В момент отправки
+     * вызывает метод submit()
+     * */
+    registerEvents() {
+        this.element.addEventListener('submit', (e) => {
+            e.preventDefault()
+        })
+    }
 
-  /**
-   * Необходимо запретить отправку формы. В момент отправки
-   * вызывает метод submit()
-   * */
-  registerEvents() {
+    /**
+     * Преобразует данные формы в объект вида
+     * {
+     *  'название поля формы 1': 'значение поля формы 1',
+     *  'название поля формы 2': 'значение поля формы 2'
+     * }
+     * */
+    getData() {
+        let obj = {};
+        for (let i = 0; i < this.element.length; i++) {
+            let key = this.element[i].name
+            let value = this.element[i].value
+            obj[key] = value
+        }
+        return obj
+    }
 
-  }
+    onSubmit(options) {
 
-  /**
-   * Преобразует данные формы в объект вида
-   * {
-   *  'название поля формы 1': 'значение поля формы 1',
-   *  'название поля формы 2': 'значение поля формы 2'
-   * }
-   * */
-  getData() {
+    }
 
-  }
-
-  onSubmit( options ) {
-
-  }
-
-  /**
-   * Вызывает метод onSubmit и передаёт туда
-   * данные, полученные из метода getData()
-   * */
-  submit() {
-
-  }
+    /**
+     * Вызывает метод onSubmit и передаёт туда
+     * данные, полученные из метода getData()
+     * */
+    submit() {
+        this.onSubmit({
+            url: this.element.action,
+            method: this.element[method],
+            data: this.getData()
+        })
+    }
 }
