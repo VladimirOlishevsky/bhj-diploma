737916bd5839ca7b8fd64c3603d55c64c37408b7
manager7 <manager7@manager7.sandsoft.ru>
2020-01-24 11:10:54 +0300

    updates

--- js/api/createRequest.js
+++ js/api/createRequest.js
@@ -3,45 +3,7 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
-    // const xhr = new XMLHttpRequest;
-    // xhr.withCredentials = true;
-
-    // if (options.method === 'GET') {
-
-    //     console.log(options)
-
-    //     xhr.open(options.method, `${options.url}?mail=${options.data.mail}&password${options.data.password}`);
-    //     xhr.send();
-    // }
-    // if (options.method === 'POST') {
-
-
-    //     let formData = new FormData;
-
-    //     console.log(options)
-    //     formData.append('mail', options.data.mail);
-    //     formData.append('password', options.data.password);
-
-    //     xhr.open(options.method, options.url);
-    //     xhr.send(formData);
-    // }
-    // if (xhr.readyState === 4) {
-    //     if (xhr.status === 200) {
-    //         options.callback(null, xhr.response);
-    //     } else {
-    //         options.callback(xhr.status, xhr.response);
-    //     }
-    // }
-    // try {
-    //     xhr.open(options.method, options.url);
-    //     xhr.send(formData);
-    // } catch (e) {
-    //     // перехват сетевой ошибки
-    //     callback(e);
-    // }
-
-    // return xhr
-
+  
     let xhr = new XMLHttpRequest();
     let formData = new FormData();
 
